30a7e33ee628e4ad47d91dee14a8c75024f4c14c
IAmNithi <kalanithi_sas@outlook.com>
2019-08-27 14:52:13 +0530

    added provider to renderer

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Routes from '../Client/Routes';
+import { Provider } from 'react-redux';
 
 
-export default (req)  => {
+export default (req, store)  => {
     const content = renderToString(
-        <StaticRouter location={req.path} context={{}}>
-            <Routes />
-        </StaticRouter>
+        <Provider store={store}>
+            <StaticRouter location={req.path} context={{}}>
+                <Routes />
+            </StaticRouter>
+        </Provider>
     );
     return `
         <html>
