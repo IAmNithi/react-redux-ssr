d4f107172ac4975bc93c326611319c105ae7e658
IAmNithi <kalanithi_sas@outlook.com>
2019-08-27 14:50:03 +0530

    added redux to client and server

--- server/src/Client/Client.js
+++ server/src/Client/Client.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
 
+const store = createStore(reducers , {} , applyMiddleware(thunk));
+
 ReactDom.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
     , document.querySelector('#root'));
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 import renderer from './helpers/renderer';
 const app = express();
-
+import createStore from './helpers/createStore';
 app.use(express.static('public'));
 app.get('*', (req,res) => {
-    res.send(renderer(req));
+    const store = createStore();
+    res.send(renderer(req, store));
 });
 app.listen(3000, () => {
     console.log('App Listening on Port 3000');
